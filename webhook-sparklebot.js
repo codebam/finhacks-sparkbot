9445c369d3550d82e060fe3d497574a6e8fda893
matthew <matthewsbarnes>
2016-11-19 19:08:26 +0000

    update webhook

--- webhook-sparklebot.js
+++ webhook-sparklebot.js
@@ -1,10 +1,21 @@
 var express = require('express')
 var app = express()
 
+var stockName = function (req, res, next) {
+    console.log("\t=================");
+    console.log("\tRequest Received!");
+    console.log("\t=================");
+    console.log(Date.now())
+    console.log(req.body);
+    next()
+}
+
+app.use(stockName)
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.listen(80, function () {
   console.log('Example app listening on port 3000!')
-})eturn stock.get_price()
+})
